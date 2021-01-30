4bcf390d0e4232f5f39a42bf6c7c98cab7608ef9
albertot_dev <atejero.dam@gmail.com>
2021-01-31 00:12:01 +0100

    added procfile

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import  GameController from './infraestructure/controllers/GameController';
 import MongoConnector from './infraestructure/mongoconnector';
 dotenv.config();
 
+const port = process.env.PORT || 3000
+
 const controllers = [
     new GameController(new MongoRepository()),
     new AuthController(new MongoRepository())
@@ -14,7 +16,7 @@ const controllers = [
 function bootstrap() {
     // tslint:disable-next-line: no-console
     MongoConnector.init({ useNewUrlParser: true });
-    const app = new App(controllers, 3000);
+    const app = new App(controllers, +port);
     app.listen();
 }
 
