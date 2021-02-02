89014ead9599e653d428cce8acae87eac0f1d70c
albertot_dev <atejero.dam@gmail.com>
2021-02-02 01:17:52 +0100

    enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,32 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { Controller } from "./infraestructure/controllers/ControllerInterface";
+import  cors  from 'cors';
 
 export default class App {
     app: express.Application;
     port: number;
 
-    constructor(controllers: Controller[], port: number){
+    constructor(controllers: Controller[], port: number) {
         this.app = express();
+        const corsOptions = {
+            origin: 'https://gameofchars.netlify.app',
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        }
+        this.app.use(cors(corsOptions))
         this.app.use(bodyParser.json());
         this.port = port;
         this.initizalizeControllers(controllers);
     }
     private initizalizeControllers(controllers: Controller[]) {
 
-        controllers.forEach(controller=>{
+        controllers.forEach(controller => {
             this.app.use('/', controller.router);
         })
     }
 
-    public listen(){
-        this.app.listen(this.port, ()=>{
+    public listen() {
+        this.app.listen(this.port, () => {
             // tslint:disable-next-line: no-console
             console.log(`Game of chars running on localhost:${this.port}...`);
         })
