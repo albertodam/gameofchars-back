992514d4f58e1f669ff0862713bdf63ead42f2ed
albertot_dev <atejero.dam@gmail.com>
2021-02-04 23:53:20 +0100

    added save all games, count ngames and max score

--- src/infraestructure/repository/MongoRepository.ts
+++ src/infraestructure/repository/MongoRepository.ts
@@ -9,28 +9,43 @@ export class MongoRepository {
         newScore.score = scoreBoard.score;
         newScore.externalId = scoreBoard.id;
         return new Promise((resolve, reject) => {
-            MongoScoreBoard
-                .findOneAndUpdate({ externalId: newScore.externalId, name: newScore.name }, { $inc: { score: newScore.score } }, { upsert: true, setDefaultsOnInsert: true })
-                .exec((err, res) => {
-                    if (err) {
-                        reject();
-                        return;
-                    }
-                    resolve();
-                })
+
+            MongoScoreBoard.create(newScore).then(() => {
+                resolve();
+            }, err => reject())
         });
     }
 
     async getScore(limit = 5): Promise<ScoreBoard[]> {
         return new Promise((resolve, reject) => {
-            MongoScoreBoard.find({externalId: {$ne:'anonymous-5854397'}}).select({ "name": 1, "_id": 0, "score":1}).sort({score: 'desc'}).exec((err, docs) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
+            MongoScoreBoard.aggregate(
+                [
+                    {
+                        $match: {
+                            'externalId': { $ne: 'anonymous-5854397' }
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: "$externalId",
+                            name: { $first: '$name' },
+                            score: { $max: "$score" },
+                            nGames: { $sum: 1 },
+                            avg: {$avg:'$score'}
+                        }
+                    }
+                ])
+                .sort({ score: 'desc' })
+                .limit(limit)
+                .exec((err, docs) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
 
-                resolve(docs as ScoreBoard[]);
-            })
+                    resolve(docs as ScoreBoard[]);
+                })
+
         })
 
     }
