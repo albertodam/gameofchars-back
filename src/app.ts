1b76e4589da7acef998935473c03da82de3e2fbf
albertot_dev <atejero.dam@gmail.com>
2021-02-04 22:28:01 +0100

    sockets works

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,28 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { Controller } from "./infraestructure/controllers/ControllerInterface";
-import  cors  from 'cors';
+import cors from 'cors';
+import { Server, Socket } from "socket.io";
 
 export default class App {
     app: express.Application;
     port: number;
+    server: any;
+    io: Server;
 
     constructor(controllers: Controller[], port: number) {
         this.app = express();
+
         const corsOptions = {
-            origin: 'https://gameofchars.netlify.app',
+            origin: '*',
             methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         }
         this.app.use(cors(corsOptions))
         this.app.use(bodyParser.json());
         this.port = port;
         this.initizalizeControllers(controllers);
+        this.server = require('http').createServer(this.app);
+        this.io = new Server(this.server, { cors: corsOptions })
     }
     private initizalizeControllers(controllers: Controller[]) {
 
@@ -26,9 +32,21 @@ export default class App {
     }
 
     public listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             // tslint:disable-next-line: no-console
             console.log(`Game of chars running on localhost:${this.port}...`);
-        })
+        });
+
+        this.io.on("connect", (socket: any) => {
+            console.log("Connected client on port %s.", this.port);
+            socket.on("message", (m: any) => {
+                console.log("[server](message): %s", JSON.stringify(m));
+                socket.broadcast.emit("message", m);
+            });
+
+            socket.on("disconnect", () => {
+                console.log("Client disconnected");
+            });
+        });
     }
 }
